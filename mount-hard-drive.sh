d5558531603da25c78b977e7e92a5bef3c7e9197
Tomi Javanainen <tomi.javanainen@gmail.com>
2020-11-16 23:22:05 +0200

    Set up mounting

--- mount-hard-drive.sh
+++ mount-hard-drive.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/bash
+# RUN THIS SCRIPT AS A NORMAL USER
+# WILL SUDO WHEN NEEDED
 
 # SETTINGS
 MOUNT_DIR_BASE=/user-mounts
@@ -46,7 +48,7 @@ echo
 sudo mkfs.ext4 -F "${TARGET_PARTITION}"
 if [ $? -ne 0 ]; then exit 2; fi
 echo
-TARGET_PARTITION_UUID=$(sudo blkid -o value -s UUID /dev/sdb1)
+TARGET_PARTITION_UUID=$(sudo blkid -o value -s UUID "${TARGET_PARTITION}")
 echo "UUID for created partition is ${TARGET_PARTITION_UUID}"
 echo
 
@@ -57,3 +59,30 @@ NODE_NAME="${HDD_VENDOR}-${HDD_SERIAL_LAST_4}"
 echo "Node name is ${NODE_NAME}"
 MOUNT_POINT="${MOUNT_DIR_BASE}/${NODE_NAME}"
 echo "Mount point is ${MOUNT_POINT}"
+sudo e2label "${TARGET_PARTITION}" "${NODE_NAME}"
+if [ $? -ne 0 ]; then exit 2; fi
+
+# MOUNTING
+echo "Creating mount point"
+mkdir -p "${MOUNT_POINT}"
+if [ $? -ne 0 ]; then exit 2; fi
+echo "Test mount"
+sudo mount -U "${TARGET_PARTITION_UUID}" "${MOUNT_POINT}"
+if [ $? -ne 0 ]; then exit 2; fi
+echo "Set owner to ${USER}:$(id -gn)"
+sudo chown -R "${USER}:$(id -gn)" "${MOUNT_POINT}"
+if [ $? -ne 0 ]; then exit 2; fi
+echo "Test file write"
+echo "test" > "${MOUNT_POINT}/test.txt"
+if [ $? -ne 0 ]; then exit 2; fi
+echo "Remove test file"
+rm "${MOUNT_POINT}/test.txt"
+if [ $? -ne 0 ]; then exit 2; fi
+echo "Unmount"
+sudo umount "${MOUNT_POINT}"
+if [ $? -ne 0 ]; then exit 2; fi
+echo "Write fstab"
+echo "UUID=${TARGET_PARTITION_UUID} ${MOUNT_POINT} ext4 nosuid,nodev,nofail,x-gvfs-show  0  2"
+
+
+
