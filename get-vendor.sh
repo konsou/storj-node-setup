bfa01befa2be8bcb888f4e3e7edad126eacc87b9
Tomi Javanainen <tomi.javanainen@gmail.com>
2020-11-16 22:10:06 +0200

    vendor string to lower case

--- get-vendor.sh
+++ get-vendor.sh
@@ -2,4 +2,4 @@
 MODEL_LINE=$(sudo fdisk -l "$1" | grep "Disk model:")
 read -ra SPLIT <<<"${MODEL_LINE}"
 VENDOR="${SPLIT[2]}"
-echo "${VENDOR}"
+echo "${VENDOR}" | tr '[:upper:]' '[:lower:]'  # to lower case
