584a0edd89720af606cb94a0c2b8c3e85763e1e2
Tomi Javanainen <tomi.javanainen@gmail.com>
2020-11-16 22:21:22 +0200

    Fix to use $1

--- get-serial-last-4.sh
+++ get-serial-last-4.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
-SERIAL=$(udevadm info --query=all --name=/dev/sdd | grep ID_SERIAL_SHORT)
+SERIAL=$(udevadm info --query=all --name="$1" | grep ID_SERIAL_SHORT)
 echo "${SERIAL: -4}" | tr '[:upper:]' '[:lower:]'  # to lower case
