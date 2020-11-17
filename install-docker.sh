59a313defd1ef5167e457dc40414c4e0020828b0
Tomi Javanainen <tomi.javanainen@gmail.com>
2020-11-17 17:25:04 +0200

    More status texts for install-docker

--- install-docker.sh
+++ install-docker.sh
@@ -4,6 +4,8 @@ echo
 echo "INSTALLING DOCKER"
 echo
 
+echo "Install prerequisites"
+echo
 sudo apt update
 if [ $? -ne 0 ]; then exit 2; fi
 
@@ -15,11 +17,14 @@ sudo apt -y install \
     software-properties-common
 if [ $? -ne 0 ]; then exit 2; fi
 
+echo
+echo "Add Docker key"
+echo
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 if [ $? -ne 0 ]; then exit 2; fi
 
 echo 
-echo "Adding docker repo. THIS MAY FAIL ON MINT, FIX"
+echo "Add docker repo. THIS MAY FAIL ON MINT, FIX"
 echo
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
@@ -27,16 +32,22 @@ sudo add-apt-repository \
    stable"
 if [ $? -ne 0 ]; then exit 2; fi
 
+echo
+echo "Install docker packages"
+echo
 sudo apt update
-sudo apt install docker-ce docker-ce-cli containerd.io
+sudo apt -y install docker-ce docker-ce-cli containerd.io
 if [ $? -ne 0 ]; then exit 2; fi
 
+echo
+echo "Test docker as root"
+echo
 # Test #1 - as root
 sudo docker run hello-world
 if [ $? -ne 0 ]; then exit 2; fi
 
 echo
-echo "Adding ${USER} to docker group"
+echo "Add ${USER} to docker group"
 echo
 sudo groupadd docker
 if [ $? -ne 0 ]; then exit 2; fi
@@ -45,6 +56,9 @@ if [ $? -ne 0 ]; then exit 2; fi
 
 newgrp docker
 if [ $? -ne 0 ]; then exit 2; fi
+echo
+echo "Test docker as regular user"
+echo
 # Test #2 - as a regular user
 docker run hello-world
 if [ $? -ne 0 ]; then exit 2; fi
