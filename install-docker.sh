c0dca7a45103cb79173c8c3e4f22fed6e729aa74
Tomi Javanainen <tomi.javanainen@gmail.com>
2020-11-17 17:12:30 +0200

    install-docker.sh

---
+++ install-docker.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/bash
+
+echo 
+echo "INSTALLING DOCKER"
+echo
+
+sudo apt update
+if [ $? -ne 0 ]; then exit 2; fi
+
+sudo apt -y install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+if [ $? -ne 0 ]; then exit 2; fi
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+if [ $? -ne 0 ]; then exit 2; fi
+
+echo 
+echo "Adding docker repo. THIS MAY FAIL ON MINT, FIX"
+echo
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+if [ $? -ne 0 ]; then exit 2; fi
+
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io
+if [ $? -ne 0 ]; then exit 2; fi
+
+# Test #1 - as root
+sudo docker run hello-world
+if [ $? -ne 0 ]; then exit 2; fi
+
+echo
+echo "Adding ${USER} to docker group"
+echo
+sudo groupadd docker
+if [ $? -ne 0 ]; then exit 2; fi
+sudo usermod -aG docker $USER
+if [ $? -ne 0 ]; then exit 2; fi
+
+newgrp docker
+if [ $? -ne 0 ]; then exit 2; fi
+# Test #2 - as a regular user
+docker run hello-world
+if [ $? -ne 0 ]; then exit 2; fi
+
+echo
+echo "DOCKER SUCCESFULLY INSTALLED"
+echo
+exit 0
