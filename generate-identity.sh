dd0def6ff3eec58347e4d6fc69ba27223b9d8147
Tomi Javanainen <tomi.javanainen@gmail.com>
2020-11-18 14:20:25 +0200

    Install unzip

--- generate-identity.sh
+++ generate-identity.sh
@@ -7,6 +7,8 @@ echo
 echo "Generating Storj identity"
 echo
 curl -L https://github.com/storj/storj/releases/latest/download/identity_linux_amd64.zip -o identity_linux_amd64.zip
+sudo apt update
+sudp apt install unzip
 unzip -o identity_linux_amd64.zip
 chmod +x ./identity
 
