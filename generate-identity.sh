b8eb647d8a438a576be271fc46d3cb095dab8645
Tomi Javanainen <tomi.javanainen@gmail.com>
2020-11-18 14:46:35 +0200

    Make identity gen script better - use variables for dir names, only download and install identity executable and unzip if needed

--- generate-identity.sh
+++ generate-identity.sh
@@ -3,15 +3,29 @@
 # exit when any command fails
 set -e
 
+IDENTITY_EXECUTABLE_DIR=./identity-executable
+IDENTITY_DIR=./identity
+
 echo
 echo "Generating Storj identity"
 echo
-mkdir -p ./identity-executable
-curl -L https://github.com/storj/storj/releases/latest/download/identity_linux_amd64.zip -o ./identity-executable/identity_linux_amd64.zip
-sudo apt update
-sudo apt -y install unzip
-unzip -o ./identity-executable/identity_linux_amd64.zip -d ./identity-executable
-chmod +x ./identity-executable/identity
+echo "Creating directories if needed"
+mkdir -p "${IDENTITY_EXECUTABLE_DIR}"
+mkdir -p "${IDENTITY_DIR}"
 
-mkdir -p ./identity
-./identity-executable/identity create storagenode --identity-dir ./identity
+if [ ! -e "${IDENTITY_EXECUTABLE_DIR}/identity" ]
+then
+    echo "Downloading identity executable"
+    curl -L https://github.com/storj/storj/releases/latest/download/identity_linux_amd64.zip -o "${IDENTITY_EXECUTABLE_DIR}/identity_linux_amd64.zip"
+    if [ $(dpkg-query -W -f='${Status}' unzip 2>/dev/null | grep -c "ok installed") -eq 0 ]
+    then
+        echo "Installing unzip"
+        sudo apt update
+        sudo apt -y install unzip
+    fi
+
+    unzip -o "${IDENTITY_EXECUTABLE_DIR}/identity_linux_amd64.zip" -d "${IDENTITY_EXECUTABLE_DIR}"
+    chmod +x "${IDENTITY_EXECUTABLE_DIR}/identity"
+fi
+
+"${IDENTITY_EXECUTABLE_DIR}/identity" create storagenode --identity-dir "${IDENTITY_DIR}"
