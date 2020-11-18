30a2e6b511d14c17f659c595e7a18c47b1919816
Tomi Javanainen <tomi.javanainen@gmail.com>
2020-11-18 15:58:40 +0200

    Ask user for needed info. Create storagenode dirs.

--- storj-node-setup.sh
+++ storj-node-setup.sh
@@ -23,9 +23,12 @@ echo "Using identity directory ${IDENTITY_DIR}"
 # Authorize identity here
 source ./authorize-identity.sh
 
-# Port numbers
+# Port numbers and other needed info
 read -p "Enter external Storj port to use: " PORT
 read -p "Enter external dashboard port to use: " DASHBOARD_PORT
+read -p "Enter wallet address: " WALLET_ADDRESS
+read -p "Enter email address: " EMAIL_ADDRESS
+read -p "Enter external ip or web address: " WEB_ADDRESS
 
 # SELECT DEVICE
 VALID_INPUT=0
@@ -105,5 +108,11 @@ sudo mount -av
 
 # INSTALL DOCKER
 ./install-docker.sh
+
+# CREATE NEEDED DIRS
+mkdir -p "${MOUNT_POINT}/storagenode"
+mkdir -p "${MOUNT_POINT}/identity"
+
+
 
 
