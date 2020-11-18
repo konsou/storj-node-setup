c17a3592f32b4d79494f7eec31d237c4bf777bf2
Tomi Javanainen <tomi.javanainen@gmail.com>
2020-11-18 14:18:13 +0200

    Start work on generating identity

---
+++ generate-identity.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# exit when any command fails
+set -e
+
+echo
+echo "Generating Storj identity"
+echo
+curl -L https://github.com/storj/storj/releases/latest/download/identity_linux_amd64.zip -o identity_linux_amd64.zip
+unzip -o identity_linux_amd64.zip
+chmod +x ./identity
+
+./identity create storagenode --identity-dir ./identity
