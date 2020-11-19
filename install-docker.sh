6e1118cea8a40345694219e522a313e7797643d1
Tomi Javanainen <tomi.javanainen@gmail.com>
2020-11-19 15:39:58 +0200

    Remove unneeded apt update - edit text a bit

--- install-docker.sh
+++ install-docker.sh
@@ -19,7 +19,6 @@ fi
 
 echo "Install prerequisites"
 echo
-sudo apt update
 
 sudo apt -y install \
     apt-transport-https \
@@ -34,7 +33,7 @@ echo
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 echo 
-echo "Add docker repo. THIS MAY FAIL ON MINT, FIX"
+echo "Add docker repo. THIS MAY FAIL IF YOU'RE NOT USING UBUNTU!"
 echo
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
@@ -44,26 +43,22 @@ sudo add-apt-repository \
 echo
 echo "Install docker packages"
 echo
-sudo apt update
 sudo apt -y install docker-ce docker-ce-cli containerd.io
 
 echo
 echo "Test docker as root"
 echo
-# Test #1 - as root
 sudo docker run hello-world
 
 echo
 echo "Add ${USER} to docker group"
 echo
 sudo usermod -aG docker $USER
-# sudo groupadd docker || true # don't exit if this fails (group may already exist)
 
 echo
 echo "Test docker as regular user"
 echo
 newgrp docker << END
-# Test #2 - as a regular user
 docker run hello-world
 END
 
