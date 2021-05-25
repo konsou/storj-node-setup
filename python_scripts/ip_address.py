ba8710391085a392afa1778e79c41fd5002473b3
Tomi Javanainen <tomi.javanainen@gmail.com>
2021-05-25 13:25:14 +0300

    Add ip_address.py

---
+++ python_scripts/ip_address.py
@@ -0,0 +1,17 @@
+import socket
+
+
+def get_local_primary_ip() -> str:
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        s.connect(('8.8.8.8', 1))
+        ip = s.getsockname()[0]
+    except OSError:
+        ip = '127.0.0.1'
+    finally:
+        s.close()
+    return ip
+
+
+if __name__ == '__main__':
+    print(get_local_primary_ip())
