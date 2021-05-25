ebd936b6eed48f37cc356e1de47b4c657326ee80
Tomi Javanainen <tomi.javanainen@gmail.com>
2021-05-25 16:38:40 +0300

    Ask user if they want to install Python 3.9 - exit otherwise

--- storj-node-setup.sh
+++ storj-node-setup.sh
@@ -9,7 +9,7 @@
 find_python() {
         set +e
         unset BEST_PYTHON_VERSION
-        for V in 3.9 39 3.8 38 3.7 37 3.6 36; do
+        for V in 3.9 39 3.8 38; do
                 if which python$V >/dev/null; then
                         if [ x"$BEST_PYTHON_VERSION" = x ]; then
                                 BEST_PYTHON_VERSION=$V
@@ -25,10 +25,20 @@ set -e
 while true; do
   find_python
   if [[ -z "$BEST_PYTHON_VERSION" ]]; then
-    echo "No Python 3 found. Installing Python 3.9 from deadsnakes PPA..."
-    sudo add-apt-repository ppa:deadsnakes/ppa
-    sudo apt update
-    sudo apt install python3.9
+    echo "No valid Python 3 found - version >= 3.8 required."
+     while true; do
+       read -p "Install Python 3.9 from deadsnakes PPA? (Y/n): " USER_INPUT
+       if [[ "${USER_INPUT}" == "y" || "${USER_INPUT}" == "Y" || "${USER_INPUT}" == "" ]]; then
+         sudo add-apt-repository ppa:deadsnakes/ppa
+         sudo apt update
+         sudo apt install python3.9 -y
+         break
+       else
+         echo "Please install Python 3.8 or newer manually to continue."
+         exit 1
+       fi
+     done
+
   else
     echo "Using Python ${BEST_PYTHON_VERSION}"
     PYTHON_EXECUTABLE=python${BEST_PYTHON_VERSION}
