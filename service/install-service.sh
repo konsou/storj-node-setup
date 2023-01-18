f5b44be9ea90d67719bce4f7e84700c494a748d9
Tomi Javanainen <tomi.javanainen@gmail.com>
2023-01-18 21:41:07 +0200

    Tweaks to install-service.sh: - check that service files exist - exit on errors

--- service/install-service.sh
+++ service/install-service.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-# Echo commands
-set -x
-
 function install_service {
   service_filename=$(ls "${1}")
   echo "Installing service ${service_filename}"
@@ -14,10 +11,26 @@ function install_service {
 
 if [ -z ${1+x} ]; then
   echo "Usage: ${0} [service_number]"
+  echo "Run inside the folder where storj service files are"
   exit 1
 fi
 
 number=$1
+service_filename="storagenode-${number}.service"
+updater_filename="storagenode-${number}-updater.service"
+
+if ! [ -f "${service_filename}" ] || ! [ -f "${updater_filename}" ]; then
+  echo "${service_filename} and/or ${updater_filename} not found. Run this script in the folder where these files are found."
+  exit 1
+else
+  echo "Service files found!"
+fi
+
+# exit when any command fails
+set -e
+# Echo commands
+set -x
+
 install_service "storagenode-${number}.service"
 install_service "storagenode-${number}-updater.service"
 
